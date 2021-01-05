9b36c6f189b474cccbda66244af2e9ffafa48631
CAILLET Alexandre <alexcaillet@free.fr>
2021-01-05 11:34:43 +0100

    classe vecteur

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "vecteur.hpp"
+
+int main(){
+}
---
+++ vecteur.hpp
@@ -0,0 +1,37 @@
+#ifndef VECTEURHPP
+#define VECTEURHPP
+
+#include <cmath>
+
+class Vec{
+public:
+    double x,y,z;
+    vec(double x1, double y1, double z1): x(x1), y(y1), z(z1){}
+    ~vec(){}
+
+    void normalize(){
+        double norme = this->norme();
+        if (norme>0){
+            x = x/norme;
+            y = y/norme;
+            z = z/norme;
+        }
+    }
+    double norme2(){
+        return x*x+y*y+z*z;
+    }
+    double norme(){
+        return sqrt(norme2());
+    }
+
+    Vec operator * (const double &f) const { return Vec(x * f, y * f, z * f); }
+    Vec operator * (const Vec &v) const { return Vec(x * v.x, y * v.y, z * v.z); }
+    Vec operator - (const Vec &v) const { return Vec(x - v.x, y - v.y, z - v.z); }
+    Vec operator + (const Vec &v) const { return Vec(x + v.x, y + v.y, z + v.z); }
+    Vec& operator += (const Vec &v) { x += v.x, y += v.y, z += v.z; return *this; }
+    Vec operator *= (const Vec &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+    Vec operator - () const { return Vec(-x, -y, -z); }
+
+};
+
+#endif
