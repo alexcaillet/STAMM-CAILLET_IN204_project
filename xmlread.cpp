0ebc3d443d94f10937e73e18cf6c593acac48b00
MarinENSTA <marin.stamm@ensta-paris.fr>
2021-02-11 18:06:07 +0100

    Lecture XML nouveaux objets

--- xmlread.cpp
+++ xmlread.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "vecteur.hpp"
-#include "tinyxml2.h"
+#include "XML/tinyxml2.h"
 #include "objet.hpp"
 #include "vector"
 
 //L'objet de ce fichier est de définir les fonctions de lecture de fichiers XML
+
+
 std::vector<Sphere> readSpheres()
 {
 	using namespace tinyxml2;
@@ -32,12 +34,12 @@ std::vector<Sphere> readSpheres()
             {
 
 
-                XMLElement *centre = sphere->FirstChildElement( "centre" );
-                if ( centre )
+                XMLElement *position = sphere->FirstChildElement( "position" );
+                if ( position )
                 {
-                    const char *x = centre->Attribute("x");
-                    const char *y = centre->Attribute("y");
-                    const char *z = centre->Attribute("z");
+                    const char *x = position->Attribute("x");
+                    const char *y = position->Attribute("y");
+                    const char *z = position->Attribute("z");
 
                     if (x)
                         ix=atof(x);
@@ -82,7 +84,7 @@ std::vector<Sphere> readSpheres()
                     itransp=atof(transp->GetText());
                 	//std::cout << transp->GetText() << std::endl;
 				
-                //spheres.push_back(Sphere(Vec(ix,iy,iz), iradius, Vec(ia, ib, ic), ireflec, itransp)); 
+                spheres.push_back(Sphere(Vec(ix,iy,iz), Vec(ia, ib, ic), ireflec, itransp, iradius)); 
 
                 sphere = sphere->NextSiblingElement( "sphere" );
             }
@@ -105,6 +107,7 @@ std::vector<Plan> readPlans()
     double ilongueur, ilargeur;
     double inormalx, inormaly, inormalz;
     double ia, ib, ic;
+    double ireflec, itransp;
  
     if( scene )
     {
@@ -118,12 +121,12 @@ std::vector<Plan> readPlans()
             {
 
 
-                XMLElement *point = plan->FirstChildElement( "point" );
-                if ( point )
+                XMLElement *position = plan->FirstChildElement( "position" );
+                if ( position )
                 {
-                    const char *x = point->Attribute("x");
-                    const char *y = point->Attribute("y");
-                    const char *z = point->Attribute("z");
+                    const char *x = position->Attribute("x");
+                    const char *y = position->Attribute("y");
+                    const char *z = position->Attribute("z");
 
                     if (x)
                         ix=atof(x);
@@ -137,22 +140,27 @@ std::vector<Plan> readPlans()
                 if (longueur)
                     ilongueur=atof(longueur->GetText());
                 	//std::cout << radius->GetText() << std::endl;
+
+                XMLElement *largeur = plan->FirstChildElement( "largeur");
+                if (largeur)
+                    ilargeur=atof(largeur->GetText());
+                    //std::cout << radius->GetText() << std::endl;
 				
 				XMLElement *normal = plan->FirstChildElement( "normal" );
                 if ( normal )
                 {
-                    const char *normalx = normal->Attribute("normalx");
-                    const char *normaly = normal->Attribute("normaly");
-                    const char *normalz = normal->Attribute("normalz");
+                    const char *x = normal->Attribute("x");
+                    const char *y = normal->Attribute("y");
+                    const char *z = normal->Attribute("z");
 
-                    if (normalx)
-                        inormalx=atof(normalx);
+                    if (x)
+                        inormalx=atof(x);
                         //std::cout << "a : " << a << "\n";
-                    if (normaly)
-                        inormaly=atof(normaly);
+                    if (y)
+                        inormaly=atof(y);
                         //std::cout << "b : " << b << "\n";
-                    if (normalz)
-                        inormalz=atof(normalz);
+                    if (z)
+                        inormalz=atof(z);
                         //std::cout << "c : " << c << "\n";
                 }
 
@@ -173,8 +181,20 @@ std::vector<Plan> readPlans()
                         ic=atof(c);
                         //std::cout << "c : " << c << "\n";
                 }
+
+                XMLElement *reflec = plan->FirstChildElement( "re");
+                if (reflec)
+                    ireflec=atof(reflec->GetText());
+                    //std::cout << reflec->GetText() << std::endl;
+
+
+                XMLElement *transp = plan->FirstChildElement( "tr");
+                if (transp)
+                    itransp=atof(transp->GetText());
+                    //std::cout << transp->GetText() << std::endl;
 				
-               	//plans.push_back(Sphere(Vec(ix,iy,iz), iradius, Vec(ia, ib, ic), ireflec, itransp)); 
+               	plans.push_back(Plan(Vec(ix,iy,iz), Vec(ia, ib, ic), ireflec, itransp,
+                                ilongueur, ilargeur, Vec(inormalx, inormaly, inormalz)));
 
                 plan = plan->NextSiblingElement( "plan" );
             }
@@ -184,9 +204,128 @@ std::vector<Plan> readPlans()
 }
 
 
+std::vector<Parallelepipede> readParall()
+{
+    using namespace tinyxml2;
+    XMLDocument doc;
+    std::vector<Parallelepipede> paralls; 
+    doc.LoadFile( "scene.xml" );
+    
+    XMLElement *scene = doc.RootElement();
+
+    double ix, iy, iz;
+    double ilongueur, ilargeur, ihauteur;
+    double ia, ib, ic;
+    double ireflec, itransp;
+    double iorix, ioriy, ioriz;
+
+ 
+    if( scene )
+    {
+        XMLElement *objects = scene->FirstChildElement( "objects" );
+ 
+        if ( objects )
+        {   
+            XMLElement *parall = objects->FirstChildElement( "parallelepipede" );
+
+            while( parall )
+            {
+
+
+                XMLElement *position = parall->FirstChildElement( "position" );
+                if ( position )
+                {
+                    const char *x = position->Attribute("x");
+                    const char *y = position->Attribute("y");
+                    const char *z = position->Attribute("z");
+
+                    if (x)
+                        ix=atof(x);
+                    if (y)
+                        iy=atof(y);
+                    if (z)
+                        iz=atof(z);
+                }
+
+                XMLElement *longueur = parall->FirstChildElement( "longueur");
+                if (longueur)
+                    ilongueur=atof(longueur->GetText());
+                    //std::cout << radius->GetText() << std::endl;
+
+                XMLElement *largeur = parall->FirstChildElement( "largeur");
+                if (largeur)
+                    ilargeur=atof(largeur->GetText());
+                    //std::cout << radius->GetText() << std::endl;
+                
+                XMLElement *hauteur = parall->FirstChildElement( "hauteur");
+                if (hauteur)
+                    ihauteur=atof(hauteur->GetText());
+                    //std::cout << radius->GetText() << std::endl;
+
+                XMLElement *orientation = parall->FirstChildElement( "orientation" );
+                if ( orientation )
+                {
+                    const char *x = orientation->Attribute("x");
+                    const char *y = orientation->Attribute("y");
+                    const char *z = orientation->Attribute("z");
+
+                    if (x)
+                        iorix=atof(x);
+                        //std::cout << "a : " << a << "\n";
+                    if (y)
+                        ioriy=atof(y);
+                        //std::cout << "b : " << b << "\n";
+                    if (z)
+                        ioriz=atof(z);
+                        //std::cout << "c : " << c << "\n";
+                    
+                }
+
+                XMLElement *scolor = parall->FirstChildElement( "surfaceColor" );
+                if ( scolor )
+                {
+                    const char *a = scolor->Attribute("a");
+                    const char *b = scolor->Attribute("b");
+                    const char *c = scolor->Attribute("c");
+
+                    if (a)
+                        ia=atof(a);
+                        //std::cout << "a : " << a << "\n";
+                    if (b)
+                        ib=atof(b);
+                        //std::cout << "b : " << b << "\n";
+                    if (c)
+                        ic=atof(c);
+                        //std::cout << "c : " << c << "\n";
+                }
+
+                XMLElement *reflec = parall->FirstChildElement( "re");
+                if (reflec)
+                    ireflec=atof(reflec->GetText());
+                    //std::cout << reflec->GetText() << std::endl;
+
+
+                XMLElement *transp = parall->FirstChildElement( "tr");
+                if (transp)
+                    itransp=atof(transp->GetText());
+                    //std::cout << transp->GetText() << std::endl;
+                
+                paralls.push_back(Parallelepipede(Vec(ix,iy,iz), Vec(ia, ib, ic), ireflec, itransp,
+                                ihauteur, ilongueur, ilargeur, Vec(iorix, ioriy, ioriz)));
+
+                parall = parall->NextSiblingElement( "parallelepipede" );
+            }
+        }
+    }
+    return paralls;
+}
+
+
 int main(int argc, char const *argv[])
 {
-	readSpheres();
-	readPlans();
+
+	std::vector<Sphere> spheres = readSpheres();
+	std::vector<Plan> plans = readPlans(); 
+    std::vector<Parallelepipede> paralls = readParall();
 	return 0;
 }
