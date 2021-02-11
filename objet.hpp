2c7f6e6dd4fed78ea19e1b72d9eb7e8ffdb1468c
MarinENSTA <marin.stamm@ensta-paris.fr>
2021-02-11 15:48:54 +0100

    Implementation constructeurs objets

--- objet.hpp
+++ objet.hpp
@@ -3,18 +3,25 @@
 
 #include "vecteur.hpp"
 class Objet{
+public:
 
     //Tout objet a besoin d'être défini par sa position, 
     //même si elle ne désigne pas toujours le même point (centre du cercle, point d'intéret...)
-    Vec position;
+	Vec position;
     Vec couleur;
     double reflectivite;
     double transparence; 
 
+	Objet (): 
+		position(Vec(0,0,0)), couleur(Vec(0,0,0)), reflectivite(0.5), transparence(0.5)
+		{}
+
+	Objet (Vec pos, Vec col,double reflec, double transp):
+		position(pos), couleur(col), reflectivite(reflec), transparence(transp)
+		{}
+
     //Méthode normale(), virtuelle pure pour s'adapter à chaque type d'objet
-    virtual Vec normale(Vec) = 0;
-
-
+    //virtual Vec normale(Vec) = 0;
 
     /*~Objet()
     {}*/
@@ -26,10 +33,23 @@ class Objet{
 class Sphere : public virtual Objet {
 public:
     double radius; //rayon du cercle
-    virtual Vec normale(Vec collidePosition) = 0;
+
+	Sphere():
+		Objet(), radius(1)
+		{}
+
+	Sphere(double rad):
+		Objet(), radius(rad)
+		{}
+
+	Sphere(Vec pos, Vec col,double reflec, double transp, double rad):
+		Objet (pos, col, reflec, transp), radius(rad)
+		{}
+
+
+   // virtual Vec normale(Vec collidePosition) = 0;
 
 };
-
 
 class Parallelepipede : public virtual Objet{
 public:
@@ -40,7 +60,22 @@ public:
 	/* Un parallelepipède rectangle ne peut pas être simplement défini par des longueurs et une position
 	Il faut également réfléchir à son orientation */
 
-	virtual Vec normale(Vec collidePosition) = 0;
+	Parallelepipede():
+		Objet(), hauteur(1), longueur(1), larg(1), orientation(Vec(0,0,0))
+		{}
+
+	Parallelepipede(double h, double lo, double la, Vec ori):
+		Objet(), hauteur(h), longueur(lo), larg(la), orientation(ori)
+		{}
+
+	Parallelepipede(Vec pos, Vec col, double reflec, double transp,
+			double h, double lo, double la, Vec ori):
+		Objet (pos, col, reflec, transp),
+		hauteur(h), longueur(lo), larg(la), orientation(ori)
+		{}
+
+
+	//virtual Vec normale(Vec collidePosition) = 0;
 };
 
 class Plan : public virtual Objet
@@ -48,15 +83,28 @@ class Plan : public virtual Objet
 public:
 	double longueur;
 	double larg;
-
 	//On définit aisément une surface par son vecteur normal
 	Vec normalVector;
 
+	Plan():
+		Objet(), longueur(1), larg(1), normalVector(Vec(0,1,0))
+		{}
+
+	Plan(double lo, double la, Vec norm):
+		Objet(), longueur(lo), larg(la), normalVector(norm)
+		{}
+
+	Plan(Vec pos, Vec col, double reflec, double transp, double lo, double la, Vec norm):
+		Objet (pos, col, reflec, transp),
+		longueur(lo), larg(la), normalVector(norm) 
+		{}	
+
+
 	//Ici, pas besoin de calcul, on connaît toujours la normale au plan
-	virtual Vec normale(Vec collidePosition)
+	/*virtual Vec normale(Vec collidePosition)
 	{
 		return normalVector;
-	}
+	}*/
 };
 
 #endif
