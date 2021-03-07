3d13fadce892707f3302b1902d92d6c1277e50ef
CAILLET Alexandre <alexcaillet@free.fr>
2021-03-07 18:27:09 +0100

    Programme ligne de commande fini

--- main_commandline.cpp
+++ main_commandline.cpp
@@ -1,7 +1,7 @@
 #include "moteur_rendu.hpp"
 
-int main(){
+int main(int argc, char const * argv[]){
     moteur_rendu *moteur = new moteur_rendu();
-    moteur->calcul("scene.xml");
+    moteur->calcul(argv[1]);
     delete moteur;
 }
